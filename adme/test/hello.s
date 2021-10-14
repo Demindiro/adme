6405e5050193a2cabbf8e7ec82acf61f044218b6
David Hoppenbrouwers <david@salt-inc.org>
2021-10-14 05:24:05 +0200

    Fix wrong assumption that registers aliases start without '$'

--- adme/test/hello.s
+++ adme/test/hello.s
@@ -1,11 +1,11 @@
-	li		t0, hello_str
-	li		t1, 8192
+	li		$t0, hello_str
+	li		$t1, 8192
 
 loop:
-	lbu		t2, 0(t0)
-	beq		t2, zero, done
-	sb		t2, 0(t1)
-	addi	t0, t0, 1
+	lbu		$t2, 0($t0)
+	beq		$t2, $zero, done
+	sb		$t2, 0($t1)
+	addi	$t0, $t0, 1
 	j		loop
 done:
 
